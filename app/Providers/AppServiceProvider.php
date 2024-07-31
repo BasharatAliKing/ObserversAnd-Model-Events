e2f56a14cdc6efbc09fdc89552aac91677c0b8cb
Basharat Ali <basharatalidogar14@gmail.com>
2024-07-31 17:15:51 +0500

    data added

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\Observers\PostObserver;
+use App\Observers\UserObserver;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        User::observe(UserObserver::class);
+        Post::observe(PostObserver::class);
+    }
+}
